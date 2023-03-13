2e9772fb61e6598d48739ed854c39da0d4b5a1da
MariuszBaran <mariusz777@gmail.com>
2023-03-13 13:40:03 +0100

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,23 @@
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 
+let myInterval = null; 
 
-startBtn.addEventListener("click", handleClick);
-stopBtn.addEventListener("click", handleClick);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-let nIntervId; // jak to należy zadeklarować ?
-
-function startBtn() {
-        if (!nIntervId) {
-      nIntervId = setInterval(getRandomHexColor, 1000);
-      buttonStart.disabled = true;
-    }
+function start() {
+  myInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor(); 
+  }, 1000);
+  startButton.disabled = true;
   }
 
-  function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+function stop() {
+  clearInterval(myInterval);
+  startButton.disabled = false;
   }
 
-function stopBtn() {
-    clearInterval(nIntervId);
-       nIntervId = null;
-       buttonStart.disabled = false;
-  }
-
+startButton.addEventListener("click", start);
+stopButton.addEventListener("click", stop);
