d798ebabfb0406243ce3f198b1b14ce7f25a3139
MariuszBaran <mariusz777@gmail.com>
2023-03-13 11:18:47 +0100

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+
+startBtn.addEventListener("click", handleClick);
+stopBtn.addEventListener("click", handleClick);
+
+let nIntervId; // jak to należy zadeklarować ?
+
+function startBtn() {
+        if (!nIntervId) {
+      nIntervId = setInterval(getRandomHexColor, 1000);
+      buttonStart.disabled = true;
+    }
+  }
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function stopBtn() {
+    clearInterval(nIntervId);
+       nIntervId = null;
+       buttonStart.disabled = false;
+  }
 
